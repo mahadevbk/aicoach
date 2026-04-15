a3b1101c4070310a5f232a89c8b37b5bf28b94d4
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-04-15 18:08:46 +0400

    Add files via upload

--- streamlit_aicoach.py
+++ streamlit_aicoach.py
@@ -59,7 +59,7 @@ st.markdown("""
         -webkit-text-fill-color: transparent;
         text-align: center;
         margin-bottom: 0px !important;
-        filter: drop-shadow(0 0 4px rgba(204, 255, 0, 0.9)); /* SHARPER OPTIC YELLOW SHADOW */
+        filter: drop-shadow(2px 2px 0px #ccff00); /* SHARP OFFSET SHADOW (NO GLOW) */
     }
     
     .hero-subtext {
