8169138f03b5f053d32d26bb8f1ec17b68c78b49
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-04-20 06:42:23 +0400

    Add files via upload

--- streamlit_aicoach.py
+++ streamlit_aicoach.py
@@ -804,7 +804,7 @@ def render_pro_stereo(p1, p2, h1, h2, f1, f2, fps):
 
 # --- UI ---
 st.markdown("<h1>Vector Victor AI</h1>", unsafe_allow_html=True)
-st.markdown("<p class='hero-sub'>Deep form Vector and Bio mechanics AI engine</p>", unsafe_allow_html=True)
+st.markdown("<p class='hero-sub'>Deep form Vector based Bio mechanics AI engine</p>", unsafe_allow_html=True)
 
 SPORT_CONFIG = {
     "TENNIS 🎾": [
